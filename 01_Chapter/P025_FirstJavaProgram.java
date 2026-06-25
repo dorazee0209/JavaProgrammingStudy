51599dba88440ad38138694487f4de9b7fe195f2
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-06-25 14:56:15 +0900

    First Java Program

---
+++ 01_Chapter/P025_FirstJavaProgram.java
@@ -0,0 +1,6 @@
+class P025_FirstJavaProgram{
+    public static void main(String[] argc){
+        System.out.println("Welcome to Java");
+        System.out.println("First Java Program");
+    }
+}
