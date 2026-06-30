d643033e6d155d749909dbcbc2b0804dbab5653b
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-06-30 18:47:31 +0900

    Bookmark

---
+++ 05_Chapter/P128_InfinityLoop.java
@@ -0,0 +1,13 @@
+public class P128_InfinityLoop {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while(true) {
+            if(num % 6 == 0 && num % 14 == 0)
+                break;
+            num++;
+        }
+        System.out.println(num);
+    }
+}
+
---
+++ 05_Chapter/P131_ForInFor.java
@@ -0,0 +1,8 @@
+public class P131_ForInFor {
+    public static void main(String[] args) {
+        for(int i = 0; i < 3; i++) {
+            
+        }
+    }
+}
+
