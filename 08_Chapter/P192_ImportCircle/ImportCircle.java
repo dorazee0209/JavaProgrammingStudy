e735af4aff2f85688a069bf806e376c6608ca527
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-07-07 04:08:43 +0900

    08_Chapter done

--- 08_Chapter/P192_ImportCircle/ImportCircle.java
+++ 08_Chapter/P192_ImportCircle/ImportCircle.java
@@ -2,7 +2,11 @@ import com.w.smart.Circle;
 
 public class ImportCircle {
     static void main(String[] args) {
+        Circle c1 = new Circle(3.5);
+        Circle c2 = new Circle(5.5);
 
+        System.out.println("Area of r = 3.5 is " + c1.getArea());
+        System.out.println("Area of r = 5.5 is " + c2.getArea());
     }
 }
 
