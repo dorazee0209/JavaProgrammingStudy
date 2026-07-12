500a1fb6233091413a8584f989a43c0218e92fa5
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-07-12 14:36:45 +0900

    13_Chapter done

--- 13_Chapter/P309_Question13-2/Answer02.java
+++ 13_Chapter/P309_Question13-2/Answer02.java
@@ -43,6 +43,9 @@ public class Answer02 {
     }
 
     public static void swapTwoArr(int[][] arr) {
-        int[] temp;
+        int[] temp = arr[arr.length-1];
+        for (int i = arr.length-1; i > 0; i--)
+            arr[i] = arr[i-1];
+        arr[0] = temp;
     }
 }
