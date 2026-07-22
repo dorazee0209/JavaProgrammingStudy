79b28f5aa1d1de79078641dec5366e750ce57ae8
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-07-22 09:55:00 +0900

    mv some files to 15_Chapter

---
+++ 15_Chapter/P332_MobileSmartPhoneRef.java
@@ -0,0 +1,31 @@
+class MobilePhone {
+    private String num;
+
+    public MobilePhone(String number) {
+        this.num = number;
+    }
+
+    public void answer() {
+        System.out.println("Hi~ from " + num);
+    }
+}
+
+class SmartPhone {
+    private String version;
+
+    public SmartPhone(String num, String version) {
+        super(num);
+        this.version = version;
+    }
+    public void playApp() {
+        System.out.println("App is running in " + version);
+    }
+}
+
+public class P332_MobileSmartPhoneRef {
+    public static void main(String[] args) {
+        SmartPhone p1 = new SmartPhone("011-123", "iOS");
+        MobilePhone p2 = new SmartPhone("010-123", "Mac");
+    }
+}
+
