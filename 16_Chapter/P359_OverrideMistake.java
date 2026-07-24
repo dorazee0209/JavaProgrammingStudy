58035ce0107715bd6c5b0c2b94d90bf5f5695564
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-07-24 13:24:27 +0900

    P359 done

---
+++ 16_Chapter/P359_OverrideMistake.java
@@ -0,0 +1,20 @@
+class ParentAdder {
+    public int add(int n1, int n2) {
+        return n1 + n2;
+    }
+}
+
+class ChildAdder extends ParentAdder {
+    public double add(double n1, double n2) {
+        System.out.println("adding...");
+        return n1 + n2;
+    }
+}
+
+public class P359_OverrideMistake {
+    public static void main(String[] args) {
+        ParentAdder adder = new ChildAdder();
+        System.out.println(adder.add(3, 4));
+//        System.out.println(adder.add(3.1, 4.1));
+    }
+}
