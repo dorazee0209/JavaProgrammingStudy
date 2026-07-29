dbaf21439d6db0a05c0d6066f74008ac8813fadb
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-07-29 16:57:53 +0900

    P392 done

---
+++ 18_Chapter/P392_ExceptionCase6.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+//import java.util.InputMismatchException;
+
+public class P392_ExceptionCase6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("a/b...a? ");
+            int a = sc.nextInt();
+            System.out.println("a/b...b? ");
+            int b = sc.nextInt();
+            System.out.printf("%d / %d = %d\n", a, b, a/b);
+        }
+        catch(ArithmeticException | InputMismatchException e) {
+            e.getMessage();
+        }
+
+        System.out.println("Bye~ ");
+    }
+}
+
