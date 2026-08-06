569a87413a7161b51281d076d4670f44a046c11a
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-08-06 16:38:47 +0900

    Question 19-1 done

--- 19_Chapter/P428_Question19-1/Answer01.java
+++ 19_Chapter/P428_Question19-1/Answer01.java
@@ -22,8 +22,69 @@
  *     }
  */
 
-public class Answer01 {
-    public static void main(String[] args) {
+class Point {
+    private int xPos;
+    private int yPos;
+    public Point(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if(this.xPos == ((Point)obj).xPos && this.yPos == ((Point)obj).yPos)
+            return true;
+        else
+            return false;
+    }
+}
+
+class Rectangle {
+    private Point upperLeft;
+    private Point lowerRight;
+    public Rectangle(int x1, int y1, int x2, int y2) {
+        upperLeft = new Point(x1, y1);
+        lowerRight = new Point(x2, y2);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this.upperLeft.equals(((Rectangle)obj).upperLeft) && this.lowerRight.equals(((Rectangle)obj).lowerRight))
+            return true;
+        else 
+            return false;
+    }
+}
+
+public class Answer01 {
+    
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+        Point p3 = new Point(12, 2);
+
+        Rectangle r1 = new Rectangle(1, 1, 1, 1);
+        Rectangle r2 = new Rectangle(1, 1, 1, 1);
+        Rectangle r3 = new Rectangle(2, 2, 2, 2);
+
+        if(p1.equals(p2))
+            System.out.println("Same contents");
+        else
+            System.out.println("Diff");
+
+        if(p1.equals(p3))
+            System.out.println("Same contents");
+        else
+            System.out.println("Diff");
+
+        if(r1.equals(r2))
+            System.out.println("Same rec");
+        else
+            System.out.println("Diff rec");
+
+        if(r1.equals(r3))
+            System.out.println("Same rec");
+        else
+            System.out.println("Diff rec");
     }
 }
