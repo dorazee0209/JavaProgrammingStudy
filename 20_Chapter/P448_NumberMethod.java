932fba4d0f6792d854f1d74a17ed6e157c41985c
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-08-09 08:42:14 +0900

    P448 done

---
+++ 20_Chapter/P448_NumberMethod.java
@@ -0,0 +1,12 @@
+public class P448_NumberMethod {
+    public static void main(String[] args) {
+        Integer num1 = new Integer(10);
+        System.out.println(num1.intValue());
+        System.out.println(num1.doubleValue());
+
+        Double num2 = new Double(3.14);
+        System.out.println(num2.intValue());
+        System.out.println(num2.doubleValue());
+    }
+}
+
