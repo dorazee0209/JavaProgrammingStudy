e71ea3be5b9d3b21367ba980513f4fe9b7af0a82
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-08-20 14:48:29 +0900

    P496 question done

--- 21_Chapter/P496_Question21-1/Answer01.java
+++ 21_Chapter/P496_Question21-1/Answer01.java
@@ -38,6 +38,37 @@
 
 public class Answer01 {
     public static void main(String[] args) {
+        DBox<String, Integer> box1 = new DBox<>();
+        box1.set("Apple", 25);
+        DBox<String, Integer> box2 = new DBox<>();
+        box2.set("Orange", 33);
+        DDBox<DBox<String, Integer>, DBox<String, Integer>> ddbox = new DDBox<>();
+        ddbox.set(box1, box2);      // 두 개의 상자를 하나의 상자에 담음
+        System.out.println(ddbox);      // 상자의 내용물 출력
+    }
+}
 
+class DBox<L, R> {
+    private L left;
+    private R right;
+    public void set(L o, R r) {
+        left = o;
+        right = r;
+    }
+    public String toString() { return left + " & " + right; }
+}
+
+class DDBox<L, R> {
+    private L ob1;
+    private R ob2;
+
+    public void set(L ob1, R ob2) {
+        this.ob1 = ob1;
+        this.ob2 = ob2;
+    }
+
+    @Override
+    public String toString() {
+        return ob1.toString() + "\n" + ob2.toString();
     }
 }
--- 21_Chapter/P496_Question21-1/Answer02.java
+++ 21_Chapter/P496_Question21-1/Answer02.java
@@ -10,6 +10,28 @@
 
 public class Answer02 {
     public static void main(String[] args) {
+        DBox<String, Integer> box1 = new DBox<>();
+        DBox<String, Integer> box2 = new DBox<>();
+        DBox<DBox<String, Integer>, DBox<String, Integer>> box3 = new DBox<>();
+
+        box1.set("Apple", 25);
+        box2.set("Orange", 33);
+
+//        System.out.println(box1);
+//        System.out.println(box2);
+
+        box3.set(box1, box2);
+        System.out.println(box3);
 
     }
+}
+
+class DBox<L, R> {
+    private L left;
+    private R right;
+    public void set(L o, R r) {
+        left = o;
+        right = r;
+    }
+    public String toString() { return left + " & " + right; }
 }
