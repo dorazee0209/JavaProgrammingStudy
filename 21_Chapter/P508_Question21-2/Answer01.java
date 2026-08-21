9e52946869ea05b7520098e75bbaab534fef9586
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-08-21 13:58:44 +0900

    P508 done

--- 21_Chapter/P508_Question21-2/Answer01.java
+++ 21_Chapter/P508_Question21-2/Answer01.java
@@ -31,7 +31,35 @@
  */
 
 public class Answer01 {
-    public static void main(String[] args) {
+    // TODO: 이 위치에 swapBox 메소드를 정의하자.
+    public static <T extends Number> void swapBox(Box<T> box1, Box<T> box2)
+    {
+        Box<T> temp = new Box<>();
+        temp.set(box1.get());
+        box1.set(box2.get());
+        box2.set(temp.get());
 
+        /*
+        * AI 개선사항: "불필요한 객체 생성"
+        * T temp = box1.get();
+        * box1.set(box2.get());
+        * box2.set(temp);
+        */
     }
+
+    public static void main(String[] args) {
+        Box<Integer> box1 = new Box<>();
+        box1.set(99);
+        Box<Integer> box2 = new Box<>();
+        box2.set(55);
+        System.out.println(box1.get() + " & " + box2.get());
+        swapBox(box1, box2);        // 정의해야 할 swapBox 메소드
+        System.out.println(box1.get() + " & " + box2.get());
+    }
+}
+
+class Box<T extends Number> {
+    private T ob;
+    public void set(T o) { ob = o; }
+    public T get() { return ob; }
 }
