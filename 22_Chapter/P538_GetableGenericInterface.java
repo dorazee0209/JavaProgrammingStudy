c6e30a934d38e5ccb34ab8b558abcc777ca6cbe4
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-08-24 13:11:49 +0900

    P538 done

---
+++ 22_Chapter/P538_GetableGenericInterface.java
@@ -0,0 +1,34 @@
+interface Getable<T> {
+    public T get();
+}
+
+// 인터페이스 Getable<T>를 구현하는 Box<T> 클래스
+class Box<T> implements Getable<T> {
+    private T ob;
+    public void set(T ob) {
+        this.ob = ob;
+    }
+    @Override
+    public T get() {
+        return ob;
+    }
+}
+
+class Toy {
+    @Override
+    public String toString() {
+        return "I am a toy.";
+    }
+}
+
+public class P538_GetableGenericInterface {
+    public static void main(String[] args) {
+        Box<Toy> box = new Box<>();
+        box.set(new Toy());
+
+        // Box<T>가 Getable<T>를 구현하므로 참조 가능
+        Getable<Toy> gt = box;
+        System.out.println(gt.get());
+    }
+}
+
