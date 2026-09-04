3ab8a2ac99ae3eebc0deaaf1e0394df0d09c0162
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-09-04 17:34:30 +0900

    P682 done

---
+++ 27_Chapter/P682_ConsumerDemo.java
@@ -0,0 +1,10 @@
+import java.util.function.Consumer;
+
+public class P682_ConsumerDemo {
+    public static void main(String[] args) {
+        Consumer<String> c = s -> System.out.println(s);
+        c.accept("Pineapple");
+        c.accept("Strawberry");
+    }
+}
+
