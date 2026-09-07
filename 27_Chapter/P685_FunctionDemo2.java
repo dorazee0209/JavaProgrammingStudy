ec661fa8f0708f71c00cfb39c8683e8cef947aac
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-09-07 14:41:59 +0900

    P685 done

---
+++ 27_Chapter/P685_FunctionDemo2.java
@@ -0,0 +1,12 @@
+import java.util.function.Function;
+
+public class P685_FunctionDemo2 {
+    public static void main(String[] args) {
+        Function<Double, Double> cti = d -> d * 0.393701;
+        Function<Double, Double> itc = d -> d * 2.54;
+
+        System.out.println("1cm = " + cti.apply(1.0) + "inch");
+        System.out.println("1inch = " + itc.apply(1.0) + "cm");
+    }
+}
+
