87aaded55daec2c0819f365b013bcd096595143f
Dorazee0209 <92316302+dorazee0209@users.noreply.github.com>
2026-09-07 17:27:03 +0900

    P695 done

---
+++ 28_Chapter/P695_ArrangeList3.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class P695_ArrangeList3 {
+    public static void main(String[] args) {
+        List<Integer> ls = Arrays.asList(1, 3, 5, 7, 9);
+        ls = new ArrayList<>(ls);
+        JustSort js = new JustSort();
+
+        Consumer<List<Integer>> c = l -> js.sort(l);
+        c.accept(ls);
+        System.out.println(ls);
+    }
+}
+
+class JustSort {
+    public void sort(List<?> lst) {
+        Collections.reverse(lst);
+    }
+}
